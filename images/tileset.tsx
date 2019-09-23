c9e92c6fbd5082a794c15674c2772060f3ac3d8b
Daniil Gorbunov <dan.gorbunov.00@yandex.ru>
2019-09-23 19:55:53 +0300

    Добавлен вспомогательный файл для JSON

---
+++ images/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="NES - Battle City JPN - General Sprites" tilewidth="16" tileheight="16" tilecount="400" columns="25">
+ <image source="NES - Battle City JPN - General Sprites.png" width="400" height="256"/>
+</tileset>
